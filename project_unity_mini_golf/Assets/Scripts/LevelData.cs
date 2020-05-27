b9789a1970dddd999b4860a8f97788f2438a71ac
Matison234 <matson234mail@gmail.com>
2020-05-27 09:13:54 +0200

    Before level manager 27.05.2020

---
+++ project_unity_mini_golf/Assets/Scripts/LevelData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "LevelData", menuName ="Minigolf/LevelData")]
+
+public class LevelData : ScriptableObject, IEquatable<LevelData>
+{
+    public string SceneName => sceneName;
+    [NaughtyAttributes.Scene]
+    [SerializeField] private string sceneName;
+
+    public int averageHit;
+
+    public bool Equals(LevelData other)
+    {
+        if(this == other)
+        {
+            return true;
+        }
+        return false;
+    }
+}
